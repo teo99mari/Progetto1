680c2475b4d6b6c636dd36cadde47021d204de16
teo99/mari <matteomariani5829@gmail.com>
2021-02-02 15:16:59 +0100

    modifiche

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,8 @@ Route::get('/person', function () {
     return view('InputPerson', ['patate' => 'ciao']);
 });
 
-Route::get('/person/prova', 'App\Http\Controllers\Person\SearchController@run');
-
-Route::post('/person/add', 'App\Http\Controllers\Person\PageController@person');
-
-Route::post('/person/update', 'App\Http\Controllers\Person\UpdateController@run');
+//Route::post('/person/add', 'App\Http\Controllers\Person\PageController@person');
 
 Route::get('/person/list', 'App\Http\Controllers\Person\ListController@run');
+
+Route::put('/person/add', 'App\Http\Controllers\Person\AddController@run');
