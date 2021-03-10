66fc2acf039d183c924c86219b9ea689bdb04a56
Andrea Cattaneo <a.cattaneo@accenture.com>
2021-03-10 09:58:33 +0100

    menu table

---
+++ database/migrations/2021_03_10_085546_create_menu_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->id();
+            $table->string('antipasto');
+            $table->string('primo');
+            $table->string('secondo');
+            $table->string('dolce');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu');
+    }
+}
