35fefadf40b0a54383c20b3d2bac8e8b8b3a178a
teo99/mari <matteomariani5829@gmail.com>
2021-03-17 17:45:50 +0100

    mod

--- app/Http/Controllers/Parola/ApiParolaController.php
+++ app/Http/Controllers/Parola/ApiParolaController.php
@@ -8,9 +8,22 @@ class ApiParolaController {
     public function run(Request $req)
     {
         $parola = $req->post();
+        $scelta = $req->get('scelta');
 
         $obj = new ParolaController();
-        $data = [] ;
+
+        $array = [];
+
+        foreach ($parola as $value) {
+        if ($scelta == 'vocali') {
+            return $obj->vocali($value);
+        } else if ($scelta == 'consonanti') {
+            return $obj->consonanti($value);
+        } else {
+            return [$obj->vocali($value), $obj->consonanti($value)];
+        }
+    }
+        $data = [];
 
         foreach ($parola as $value) {
         $data [$value] = [
