6531536748adefbdcb0731edb9b61a03e7233048
Andrea Cattaneo <a.cattaneo@accenture.com>
2021-03-17 14:25:20 +0100

    fix

--- app/Http/Controllers/IdentityCardGenerator/IdentityCard.php
+++ app/Http/Controllers/IdentityCardGenerator/IdentityCard.php
@@ -2,45 +2,28 @@
 
 namespace App\Http\Controllers\IdentityCardGenerator;
 
+use DateTime;
+
 class IdentityCard
 {
-    public function identificationName($name){
-        $Charact = [];
-        $strN = str_split($name, 3);
-        $Charact = $strN[0];
-        return $Charact;
+    public function getStringSplittedByNumber($string, $length) {
+        $result = str_split($string, $length);
+        return $result[0];
     }
 
-    public function identificationSurname($surname){
-        $Charact = [];
-        $strS = str_split($surname, 3);
-        $Charact = $strS[0];
-        return $Charact;
+    public function yearsOld($birthYear) {
+        $date = new DateTime('now');
+        $currentDate = (int) $date->format('Y');
+
+        return $currentDate - $birthYear;
     }
 
-    public function yearsOld($yearsOld){
-
-        $years = 0;
-        $currencyYear = 2021;
-        $years = $currencyYear - $yearsOld;
-
-        return $years;
-    }
-
-    public function legal($birthYear)
-    {
+    public function legal($birthYear) {
         $var = 'minorenne';
-        $currencyYear = 2021;
-        if (($currencyYear - $birthYear) >= 18) {
+        if ($this->yearsOld($birthYear) >= 18) {
             $var = 'maggiorenne';
         }
-        return $var;
-    }
 
-    public function province($province){
-        $Charact = [];
-        $str = str_split($province, 3);
-        $Charact = $str[0];
-        return $Charact;
+        return $var;
     }
 }
--- app/Http/Controllers/IdentityCardGenerator/IdentityCardApiController.php
+++ app/Http/Controllers/IdentityCardGenerator/IdentityCardApiController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 class IdentityCardApiController
 {
     public function run(Request $req){
-
         $name = $req->get('name');
         $surname = $req->get('surname');
         $yearsOld = $req->get('BornYear');
@@ -15,11 +14,12 @@ class IdentityCardApiController
         $province = $req->get('province');
 
         $obj = new IdentityCard();
+
         return [
-        'Identification' => $obj->identificationName($name) . $obj->identificationSurname($surname),
-        'YearsOld' => $obj->yearsOld($yearsOld),
-        'birthYear' => $obj->legal($birthYear),
-        'province' => $obj->province($province)
-       ];
+            'Identification' => $obj->getStringSplittedByNumber($name, 3) . $obj->getStringSplittedByNumber($surname, 3),
+            'YearsOld' => $obj->yearsOld($yearsOld),
+            'birthYear' => $obj->legal($birthYear),
+            'province' => $obj->getStringSplittedByNumber($province, 3)
+        ];
     }
 }
